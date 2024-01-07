ce8f73276c2b92d04813daf7579df3e59e665500
Samiya-Nidhi <103763905+Samiya-Nidhi@users.noreply.github.com>
2024-01-07 12:49:50 +0600

    Add files via upload

---
+++ Robotics School Management/js/script.js
@@ -0,0 +1,14 @@
+
+$('.main-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-angle-left left"></i>', //classname left
+    nextArrow: '<i class="fa-solid fa-angle-right right"></i>' //classname right
+
+    
+  });
+  $('.banner-slide').slick({
+   arrows: false,
+   dots: true,
+  });
